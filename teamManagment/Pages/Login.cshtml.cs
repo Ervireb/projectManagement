716d3fff3653e4645fd98087a39e037667058937
Ervireb <77449663+Ervireb@users.noreply.github.com>
2023-10-27 19:33:46 +0300

    teamManagment_(new_folder)

---
+++ teamManagment/Pages/Login.cshtml.cs
@@ -0,0 +1,18 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel;
+using System.Drawing;
+using System.Reflection;
+using System.Xml.Linq;
+using System;
+
+namespace teamManagment.Pages
+{
+    public class loginModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
+
---
+++ teamManagment/Pages/Web.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace teamManagment.Pages
+{
+    public class webModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
